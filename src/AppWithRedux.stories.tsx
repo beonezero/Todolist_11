2c595d55ba65538db4ac47c13bc22ed3726be63b
Neakhai Yauheni <beonezero@protonmail.com>
2023-08-21 23:52:17 +0300

    stories: AppWithRedux.stories.tsx, ReduxStoreProviderDecorator.tsx

--- src/AppWithRedux.stories.tsx
+++ src/AppWithRedux.stories.tsx
@@ -1,13 +1,14 @@
 import type {Meta, StoryObj} from '@storybook/react';
-import {action} from "@storybook/addon-actions"
 import React from "react";
-import Task from "./Task";
-import {EditableSpan} from "./EditableSpan";
+import AppWithRedux from "./AppWithRedux";
+import {Provider} from "react-redux";
+import {store} from "./state/store";
+import {ReduxStoreProviderDecorator} from "./state/ReduxStoreProviderDecorator";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-const meta: Meta<typeof EditableSpan> = {
-    title: 'TODOLISTS/EditableSpan',
-    component: EditableSpan,
+const meta: Meta<typeof AppWithRedux> = {
+    title: 'TODOLISTS/AppWithRedux',
+    component: AppWithRedux,
     parameters: {
         // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
         layout: 'centered',
@@ -15,17 +16,13 @@ const meta: Meta<typeof EditableSpan> = {
     // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
     tags: ['autodocs'],
     // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-    argTypes: {
-        onChange: action("onChange"),
-        value: {defaultValue: "fefefe"}
-    }
+    decorators: [ReduxStoreProviderDecorator]
 };
 
 export default meta;
-type Story = StoryObj<typeof Task>;
+type Story = StoryObj<typeof AppWithRedux>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const EditableSpanStory: Story = {
-
+export const AppWithReduxStory: Story = {
+    render: args => <AppWithRedux/>
 };
-
